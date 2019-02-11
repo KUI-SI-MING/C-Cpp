3b57f91ff36b0999b57bbb2ca616858ab0f04a93
KUI-SI-MING <kuisi5495@163.com>
2019-02-11 23:28:18 +0800

    Annotation conversion

---
+++ Annotation_conversion/Annotation_conversion/CommentConvert.h
@@ -0,0 +1,25 @@
+#ifndef __COMMENTCOMVERT_H__
+#define __COMMENTCOMVERT_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+enum State
+{
+	NUL_STATE,
+	C_STATE,
+	CPP_STATE,
+	END_STATE
+};
+
+
+void CommentComvert(FILE *psInt, FILE* psOut);
+void DoNulstate(FILE* psInt, FILE * psOut, enum State *s);
+void DoCstate(FILE* psInt, FILE * psOut, enum State *s);
+void DoCppstate(FILE* psInt, FILE * psOut, enum State *s);
+void DoEndstate(enum State *s);
+
+
+
+#endif//__COMMENTCOMVERT_H__
